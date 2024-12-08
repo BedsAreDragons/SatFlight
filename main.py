2acba1618cb27777bceffcbbdb61d1c2a2bce2d0
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-12-08 21:27:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ def get_pixels():
         return jsonify({'error': 'Failed to retrieve the image'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
