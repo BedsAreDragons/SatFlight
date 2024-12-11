5957acbaa3cff478b7e35c92bca3d30bdaec7b01
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-12-11 17:47:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,33 @@ app = Flask(__name__)
 
 @app.route('/get_pixels', methods=['POST'])
 def get_pixels():
-    data = request.get_json()
-    latitude = data['latitude']
-    longitude = data['longitude']
-    offset = 0.005
+    try:
+        data = request.get_json()
+        latitude = float(data['latitude'])
+        longitude = float(data['longitude'])
+        offset = 0.005
 
-    xmin = longitude - offset
-    ymin = latitude - offset
-    xmax = longitude + offset
-    ymax = latitude + offset
+        xmin = longitude - offset
+        ymin = latitude - offset
+        xmax = longitude + offset
+        ymax = latitude + offset
 
-    url = (
-        'https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/export?'
-        'bbox={xmin},{ymin},{xmax},{ymax}'
-        '&bboxSR=4326'
-        '&size=800,800'
-        '&imageSR=4326'
-        '&format=png32'
-        '&f=image'
-    ).format(xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax)
+        url = (
+            'https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/export?'
+            'bbox={xmin},{ymin},{xmax},{ymax}'
+            '&bboxSR=4326'
+            '&size=800,800'
+            '&imageSR=4326'
+            '&format=png32'
+            '&f=image'
+        ).format(xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax)
 
-    response = requests.get(url, stream=True)
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
 
-    if response.status_code == 200:
         img = Image.open(BytesIO(response.content))
 
-        small = img  # .resize((800, 800), resample=Image.BILINEAR)
+        small = img.resize((800, 800), resample=Image.BILINEAR)
         pixelated_img = small.resize(img.size, Image.NEAREST)
 
         pixelated_img = pixelated_img.convert('RGB')
@@ -42,8 +43,9 @@ def get_pixels():
         pixels = [list(pixel) for pixel in pixel_data]
 
         return jsonify(pixels), 200
-    else:
-        return jsonify({'error': 'Failed to retrieve the image'}), 500
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
