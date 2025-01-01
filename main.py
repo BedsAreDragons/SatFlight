be303276d67e99d1463d771f600eec296d6b1c31
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2025-01-01 21:47:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def get_pixels():
         data = request.get_json()
         latitude = float(data['latitude'])
         longitude = float(data['longitude'])
-        offset = 0.02  # Updated offset size
+        offset = 0.005  # Updated offset size
 
         xmin, ymin, xmax, ymax = get_bbox(latitude, longitude, offset)
 
